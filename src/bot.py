70690ad6cb69f10dfeec86dcc89f67ba73c11646
Yarmoshch <yaroslav2006@inbox.ru>
2025-09-18 21:38:27 +0300

    Fix formatting and add pyproject.toml

--- src/bot.py
+++ src/bot.py
@@ -9,8 +9,7 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup,
-                           ReplyKeyboardRemove)
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.date import DateTrigger
 
